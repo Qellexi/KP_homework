5b87bbe020b3bd7b6959b54e0fbeb032c23839df
Qellexi <99039851+Qellexi@users.noreply.github.com>
2024-08-12 01:17:10 +0300

    Add files via upload

---
+++ кп22/src/main.js
@@ -0,0 +1,27 @@
+import 'C:/Users/Samsung/PhpstormProjects/kp_it_hillel/kp/20_lec/src/styles.scss';
+
+$(document).ready(function() {
+    const $form = $('.js--form');
+    const $input = $('.js--form__input');
+    const $todosWrapper = $('.js--todos-wrapper');
+    const $taskModal = $('#taskModal');
+    const $modalTaskContent = $('#modalTaskContent');
+
+    // Додавання нового завдання
+    $form.on('submit', function(event) {
+        event.preventDefault();
+        const taskText = $input.val().trim();
+        if (taskText) {
+            const $newTask = $(`<li class="list-group-item todo-item">${taskText}</li>`);
+            $todosWrapper.append($newTask);
+            $input.val(''); // Очищення поля вводу
+        }
+    });
+
+    // Відображення тексту завдання в модальному вікні
+    $todosWrapper.on('click', '.todo-item', function() {
+        const taskText = $(this).text();
+        $modalTaskContent.text(taskText);
+        $taskModal.modal('show');
+    });
+});
